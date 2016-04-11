798aacb2011c60749bba1ba7215109c3ac19cd96
MrMendonez <dmendonez@gmail.com>
2016-04-10 20:12:36 -0400

    got carousel to appear in modal but transitions are not smooth. must investigate further

--- staging-revamp/js/scripts.js
+++ staging-revamp/js/scripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
+
+  // Slider
   $('.slider').slider({full_width: true});
+
+  // Mobile Nav
+  $(".button-collapse").sideNav();
+
+  // Modal
+  $('.modal-trigger').leanModal();
+
+  
+
 });
